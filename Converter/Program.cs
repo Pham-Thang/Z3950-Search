4ca3f2296893fed03fcbf1301c821cdbeacffe23
Pham-Thang <phamthang20102000@gmail.com>
2023-03-15 19:53:09 +0700

    Thêm project convert

---
+++ Converter/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Converter.Data;
+using MARC;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+Console.WriteLine("Hello, World!");
+
+// Create Stream
+//var stream = new MemoryStream();
+//var writer = new StreamWriter(stream);
+//writer.Write(MARCXMLString.Single);
+//writer.Flush();
+//stream.Position = 0;
+XDocument doc = new XDocument(MARCXMLString.Single);
+
+var reader = new FileMARCXML(doc);
+
+foreach (var element in reader)
+{
+    Console.WriteLine(JsonConvert.SerializeObject(element));
+}
+//reader.GetEnumerator();
